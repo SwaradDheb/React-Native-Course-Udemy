7c417413ed72126fd77b9dcbfb1e768f02571fc5
PrasadDheb <Prasad.Dheb@crossasyst.com>
2023-01-14 22:31:04 +0530

    Make Item Deletable On Tap by handling event and using Pressable

--- App.js
+++ App.js
@@ -7,17 +7,22 @@ export default function App() {
   const [courseGoals,setCourseGoals] = useState([]);
   
    function addGoalHandler(enteredGoalText){
-    setCourseGoals((currentCourseGoals)=>[...currentCourseGoals,{text:enteredGoalText,key:Math.random().toString()},])
+    setCourseGoals((currentCourseGoals)=>[...currentCourseGoals,{text:enteredGoalText,id:Math.random().toString()},])
+   }
+   function deleteGoalHandler(id){
+    setCourseGoals(currentCourseGoals=>{
+      return currentCourseGoals.filter((goal)=>goal.id!==id);
+    });
    }
   return (
     <View style={styles.appContainer}>
      <GoalInput onAddGoal={addGoalHandler}/>
       <View style={styles.goalsContainer}>
         <FlatList data={courseGoals} renderItem={(itemData) => {
-          return <GoalItem text={itemData.item.text}/>;
+          return <GoalItem text={itemData.item.text} id={itemData.item.id} onDeleteItem={deleteGoalHandler} />;
           }}
           keyExtractor={(item,index) =>{
-            return item.key;
+            return item.id;
           }}
             alwaysBounceVertical={false}
           />
